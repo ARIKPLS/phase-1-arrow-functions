1a322ecc27b826db3119ff3b29b0ceb0fab03793
EricVentura <ericvent@outlook.com>
2023-04-06 19:55:03 -0400

    pass

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1, num2) => {
+    return (num1/num2)
+}
+
+
+const square = (num3) => {
+    return Math.pow(num3, 2)
+}
+
+const add = (num4,num5) => {
+    return (num4 + num5)
+}
